0df70c4b638a5e297d6ca77858e0adce84467516
Fabian Schmitthenner <fabian@fabian.(none)>
2011-11-16 22:11:02 +0100

    remove

--- kern/element_ptr.ext.h
+++ kern/element_ptr.ext.h
@@ -11,16 +11,18 @@ ElementPtr namedVariable (const Str &string) {
 
 template< class Str >
 ElementPtr namedFunction (const Str &string, ElementPtr var, ElementPtr f) {
-  f = f->step(0);
-  f = f->replaceNamed (variable(0, var)->copy(), 5, &string);
-  return function(var, f);
+  ElementPtr f1 = f->step(0);
+  ElementPtr f3 = f1->replaceNamed (variable(0, var)->copy(), 5, &string);
+  return function(var, f3);
 }
 
 template< class Str >
 ElementPtr namedForAll (const Str &string, ElementPtr var, ElementPtr f) {
-  f = f->step(0);
-  f = f->replaceNamed (variable (0, var)->copy(), 5, const_cast< Str * > (&string));
-  return forAll(var, f);
+  ElementPtr f1 = f->step(0);
+  Element* vc = variable (0, var)->copy();
+  ElementPtr f2 = f1->replaceNamed (vc, 5, const_cast< Str * > (&string));
+  vc->remove();
+  return forAll(var, f2);
 }
 
 };
